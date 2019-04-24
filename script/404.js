a51e9ceb96a3d18782475e218097ffd9bd4c583a
Duponin <pwet@dupon.in>
2019-04-24 22:41:31 +0200

    New error image

--- script/404.js
+++ script/404.js
@@ -1,3 +1,3 @@
-var imagesArray = ["/error/404/1.png","/error/404/2.jpg"];
-var num = Math.floor(Math.random() * 2);
+var imagesArray = ["/error/404/1.png","/error/404/2.jpg","/error/404/3.png"];
+var num = Math.floor(Math.random() * 3);
 document.canvas.src = imagesArray[num];
